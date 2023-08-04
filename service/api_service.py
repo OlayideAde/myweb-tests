af152212a5c5a3a3b11cfa07a458026ade4bb99d
olayide-paystack <laide.ademeso@gmail.com>
2023-08-04 11:27:56 +0100

    format api_service file

--- service/api_service.py
+++ service/api_service.py
@@ -1,16 +1,12 @@
-import requests 
+import requests
+
 
 class API:
     def __init__(self, url: str):
         self.url = url
 
-    def request(self, http_method:str, endpoint:str):
+    def request(self, http_method: str, endpoint: str):
         response = requests.request(http_method, f'{self.url}/{endpoint}')
         if response.ok:
             return response
         return None
-    
-
-
-
-    
--- tests/step_defs/test_view_users_steps.py
+++ tests/step_defs/test_view_users_steps.py
@@ -1,72 +1,78 @@
 import pytest
+import unittest
+import requests
 import json
 from service.api_service import API
 from unittest.mock import MagicMock, patch
 from pytest_bdd import scenario, given, when, then, parsers
 
+
 @pytest.fixture(scope='session')
 def context():
     return {}
 
-@scenario('../features/view_users.feature', 'API admin user can get list of users')
 
+@scenario('../features/view_users.feature', 'API admin user can get list of users')
 def scenario_api():
-    print("Tests finished")
+    #    print("Tests finished")
     pass
+
 
 @given("I am logged in as admin user")
 def test_admin_user_is_logged_in():
     """Admin user is logged in"""
 
+
 @when("I request API GET api/v1/users")
-@patch('mywebtests.services.api.requests.get')
-def test_request_users(self, mock_requests):
-    
+@patch('mywebtests.service.api_service.requests.get')
+def test_request_users(self):
     mock_response = MagicMock()
     mock_response.return_value.ok = True
     mock_response.status_code = 200
-    with open('../resources/users.json') as users:
+    with open('./tests/resources/users.json') as users:
         mock_response.json.return_value = json.loads(users.read())
-     
-    mock_requests.get.return_value = mock_response
-    #call service with mock
+
+    # mock_api_call = mock_response
+    # call service with mock
     api_service = API('api.myweb.com')
     response = api_service.request('GET', 'api/v1/users')
-    #store response body and status code for next steps
+    # store response body and status code for next steps
     context['response_body'] = response.json.return_value
-    context['status_code'] = response.status_code    
-    #assert request is ok
-    self.assertIsNotNone(response)    
-    
+    context['status_code'] = response.status_code
+    # assert request is ok
+    self.assertIsNotNone(response)
+
+
 @then("I get HTTP 200 status code")
 def verify_status_code(self):
     self.assertEqual(context['status_code'], 200)
 
-    
+
 @then("I get JSON in response body")
 def verify_response_body(self):
     with open('../resources/users.json') as users:
         self.assertEqual(context['response_body'], json.loads(users.read()))
-      
+
 
 @then("the HTTP response body contains a page with 4 users")
 def verify_response_length():
     """"""""
+
 
 @then(parsers.parse('1 of them is "{phrase}"'))
 def verify_admin_is_in_user_list():
     """"""""
 
 
-#@scenario('../features/view_users.feature', 'Frontend admin users can get list of users')
+# @scenario('../features/view_users.feature', 'Frontend admin users can get list of users')
 
-#def test_scenario_browser():
+# def test_scenario_browser():
 #    pass
 
-#@given('I am logged in as admin user') 
-        
-#@when('I visit the Users page') 
+# @given('I am logged in as admin user')
 
-#@then('4 users are listed') 
+# @when('I visit the Users page')
 
-#@And('1 of them is "Administrator" role') 
+# @then('4 users are listed')
+
+# @And('1 of them is "Administrator" role')
