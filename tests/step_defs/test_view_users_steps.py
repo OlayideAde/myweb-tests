a1e6cf65791e9c97364cb25c9a2abbd508edd858
olayide-paystack <laide.ademeso@gmail.com>
2023-08-04 18:59:28 +0100

    Update tests

--- tests/step_defs/test_view_users_steps.py
+++ tests/step_defs/test_view_users_steps.py
@@ -7,8 +7,8 @@ from pytest_bdd import scenario, given, when, then, parsers
 
 
 @pytest.fixture(scope='session')
-def context():
-    return {}
+def step_context():
+    return {'response': None}
 
 
 @scenario('../features/view_users.feature', 'API admin user can get list of users')
@@ -24,7 +24,7 @@ def test_admin_user_is_logged_in():
 
 @when("I request API GET api/v1/users")
 @patch('service.api_service.API.request')
-def test_request_users(mock_requests):
+def test_request_users(mock_requests, step_context):
 
     mock_response = MagicMock()
     mock_response.return_value.ok = True
@@ -36,19 +36,21 @@ def test_request_users(mock_requests):
 
     # call service with mock
     api_service = API('http://api.myweb.com')
+
     response = api_service.request('GET', 'api/v1/users')
+    step_context['response'] = response
     assert response.ok, "api call successful"
 
 
 @then("I get HTTP 200 status code")
-def verify_status_code(self):
-    self.assertEqual(context['status_code'], 200)
+def test_verify_status_code(step_context):
+    assert step_context['response'].status_code == 200
 
 
 @then("I get JSON in response body")
-def verify_response_body(self):
-    with open('../resources/users.json') as users:
-        self.assertEqual(context['response_body'], json.loads(users.read()))
+def test_verify_response_body(step_context):
+    with open(os.getcwd()+'/tests/resources/users.json') as users:
+        assert step_context['response'].json.return_value == users
 
 
 @then("the HTTP response body contains a page with 4 users")
